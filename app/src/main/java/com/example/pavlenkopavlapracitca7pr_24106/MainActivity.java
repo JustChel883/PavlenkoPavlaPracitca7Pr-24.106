3f8c1f9f8377efb2cb191900440c3aa72e160039
JustChel883 <horeksid@mail.ru>
2025-11-16 15:33:35 +0700

    made a transition from first activity to second

--- app/src/main/java/com/example/pavlenkopavlapracitca7pr_24106/MainActivity.java
+++ app/src/main/java/com/example/pavlenkopavlapracitca7pr_24106/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.pavlenkopavlapracitca7pr_24106;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.CheckBox;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setBackgroundColor(ContextCompat.getColor(this, R.color.primary));
+    }
+
+
+    public void onCheckboxClicked(View view) {
+        CheckBox checkBox = (CheckBox) view;
+
+        if (checkBox.isChecked()) {
+
+            checkBox.setBackgroundColor(ContextCompat.getColor(this, R.color.primary_light));
+        } else {
+
+            checkBox.setBackgroundColor(ContextCompat.getColor(this, R.color.primary));
+        }
+    }
+
+
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(this, account_page.class);
+        startActivity(intent);
     }
 }
